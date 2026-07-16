764a432a3985a67267dac782617216abff8af6a3
fakhririfan86 <fakhririfan86@gmail.com>
2026-07-16 13:19:38 +0700

    config cors

--- config/cors.php
+++ config/cors.php
@@ -2,24 +2,22 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:5173', 'http://127.0.0.1:5173', 'https://fe-libweb.vercel.app'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+
+        'https://fe-libweb.vercel.app',
+
+        // Production
+        'https://libweb.my.id',
+        'https://www.libweb.my.id',
+    ],
 
     'allowed_origins_patterns' => [],
 
